d122c3247aa67a847bbf75757aae9137745bf351
Dan Nixon <dan@dan-nixon.com>
2025-12-16 11:35:30 +0000

    wip

--- src/openrouter.rs
+++ src/openrouter.rs
@@ -1,41 +1,69 @@
+use itertools::Itertools;
 use miette::IntoDiagnostic;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
+use tracing::debug;
 
-#[derive(Debug, Serialize)]
-struct OpenRouterRequest {
-    model: String,
-    messages: Vec<Message>,
+#[derive(Debug, Deserialize)]
+struct Response {
+    output: Vec<Output>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+impl Response {
+    fn result_text(self) -> miette::Result<String> {
+        let message = self
+            .output
+            .iter()
+            .find(|o| matches!(o, Output::Message(_)))
+            .ok_or_else(|| miette::miette!("no message in result"))?;
+
+        if let Output::Message(message) = message {
+            let text = message.content.iter().map(|c| c.text.clone()).join("\n");
+            Ok(text)
+        } else {
+            unreachable!();
+        }
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(tag = "type")]
+enum Output {
+    Reasoning,
+    Message(Message),
+}
+
+#[derive(Debug, Deserialize)]
 struct Message {
-    role: String,
-    content: String,
+    content: Vec<MessageContent>,
 }
 
 #[derive(Debug, Deserialize)]
-struct OpenRouterResponse {
-    choices: Vec<Choice>,
-}
-
-#[derive(Debug, Deserialize)]
-struct Choice {
-    message: Message,
+struct MessageContent {
+    text: String,
 }
 
 pub async fn request(
     http_client: &reqwest::Client,
     api_key: &str,
     model: &str,
+    system_prompt: &str,
     prompt: &str,
 ) -> miette::Result<String> {
-    let request = OpenRouterRequest {
-        model: model.to_string(),
-        messages: vec![Message {
-            role: "user".to_string(),
-            content: prompt.to_string(),
-        }],
-    };
+    let request = serde_json::json!({
+        "input": [
+            {
+                "type": "message",
+                "role": "system",
+                "content": system_prompt,
+            },
+            {
+                "type": "message",
+                "role": "user",
+                "content": prompt,
+            },
+        ],
+        "model": model,
+    });
 
     let response = http_client
         .post("https://openrouter.ai/api/v1/chat/completions")
@@ -48,28 +76,9 @@ pub async fn request(
         .error_for_status()
         .into_diagnostic()?;
 
-    let response_data: OpenRouterResponse = response.json().await.into_diagnostic()?;
+    debug!("Response: {response:?}");
 
-    if let Some(choice) = response_data.choices.first() {
-        Ok(choice.message.content.clone())
-    } else {
-        Err(miette::miette!("No response"))
-    }
+    let response: Response = response.json().await.into_diagnostic()?;
+
+    response.result_text()
 }
-
-// curl -X POST https://openrouter.ai/api/v1/responses \
-//      -H "Authorization: Bearer sk-or-v1-xxx" \
-//      -H "Content-Type: application/json" \
-//      -d '{
-//   "input": [
-//     {
-//       "role": "system",
-//       "content": "You are answering single questions about a hackspace named Maker Space. You must only use information from www.makerspace.org.uk and wiki.makerspace.org.uk. Do not give the user any follow up options, just present what you find and cease communication. If you cannot find any relevant information, suggest the user asks other members and updates the wiki when they find their answer."
-//     },
-//     {
-//       "role": "user",
-//       "content": "when is the space opne?"
-//     }
-//   ],
-//   "model": "openai/gpt-5-mini:online"
-// }'
